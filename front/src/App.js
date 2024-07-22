d1121ff6588199c750ea0db119605bce25de0b5a
iSeby2002 <damiansebastian2002@gmail.com>
2024-07-23 00:03:56 +0300

    am facut login si partial registar

---
+++ front/src/App.js
@@ -0,0 +1,59 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import "./styles.css"
+import Login from "./components/loginComp/Login";
+import Register from "./components/loginComp/Register";
+// import ManageUsers from "./components/manageUsers/ManageUsers";
+// import About from "./components/defaultPages/about/About";
+// import Contact from "./components/defaultPages/contact/Contact";
+// import Home from "./components/defaultPages/home/Home";
+// import EditUsers from "./admin/users/EditUsers";
+// import DoctorAppoiment from "./components/med/appoiment/DoctorAppoiment";
+// import FormComponent from "./components/med/appoimentForm/FormComponent";
+// import Disease from "./components/med/disease/Disease";
+// import Calendar from "./components/calendar/Calendar";
+// import ManageRooms from "./components/admin/ManageRooms";
+// import ManageVehicles from "./components/admin/ManageVehicles";
+// import PatientForm from "./components/med/appoimentForm/patientForm/PatientForm";
+// import CereriPacienti from "./components/med/appoimentForm/formPages/CereriPacienti";
+// import { FormProvider } from "./components/med/appoimentForm/formComponents/FormContext";
+// import { ProtectedRoute } from "./components/navbar/ProtectedRoute";
+// import ResetPassword from "./components/loginComp/ResetPassword";
+
+
+function App() {
+    return (
+        //<FormProvider>
+            <div className="App">
+                <Router>
+                    <Routes>
+                        {/*<Route path="/" element={<Home />} />*/}
+                        {/*<Route path="/about" element={<About />} />*/}
+                        {/*<Route path="/contact" element={<Contact />} />*/}
+                        <Route path="/" element={<Login />} />
+                        {/*<Route path="/reset" element={<ResetPassword />} />*/}
+                        <Route path="/register" element={<Register />} />
+                        {/*<Route element = {<ProtectedRoute expectedRoles={["PATIENT","DOCTOR","SURGEON","ASSISTANT","SECRETARY"]}/>} >*/}
+                        {/*    <Route path="/calendar" element = {<Calendar />}/>*/}
+                        {/*</ Route>*/}
+                        {/*<Route element = {<ProtectedRoute expectedRoles={["ADMIN"]}/>} >*/}
+                        {/*    <Route path="/admin/users" element={<EditUsers />} />*/}
+                        {/*    <Route path="/admin/rooms" element = {<ManageRooms />}/>*/}
+                        {/*    <Route path="/admin/vehicle" element = {<ManageVehicles />}/>*/}
+                        {/*</ Route>*/}
+                        {/*<Route element = {<ProtectedRoute expectedRoles={["DOCTOR","SURGEON","ASSISTANT","SECRETARY"]}/>} >*/}
+                        {/*    <Route path="/med/appoiment" element = {<DoctorAppoiment />}/>*/}
+                        {/*    <Route path="/med/requests" element = {<CereriPacienti />}/>*/}
+                        {/*    <Route path="/med/form" element = {<FormComponent />}/>*/}
+                        {/*    <Route path="/med/disease" element = {<Disease />}/>*/}
+                        {/*</ Route>*/}
+                        {/*<Route element = {<ProtectedRoute expectedRoles={["PATIENT"]}/>} >*/}
+                        {/*    <Route path="/patient/appointment" element = {<PatientForm />}/>*/}
+                        {/*</Route>*/}
+                    </Routes>
+                </Router>
+            </div>
+        //</FormProvider>
+    );
+}
+
+export default App;
---
+++ front/src/components/loginComp/Login.js
@@ -0,0 +1,99 @@
+import React, { useEffect } from "react";
+import "./Login.css";
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+// import { authorize, sendEmailReset } from "../../services/LoginService";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+
+function Login() {
+    const [email, setEmail] = useState("");
+    const [parola, setParola] = useState("");
+    const [gresit, setGresit] = useState(false);
+    let navigate = useNavigate();
+
+    useEffect(() => {
+        localStorage.setItem("user", JSON.stringify(""));
+    });
+
+    const handleSubmit = (e) => {
+        // authorize(email, parola).then((res) => {
+        //     console.log(res.data);
+        //     if (res.data === "Nu s-a gasit") {
+        //         setGresit(true);
+        //         toast.error("Credențiale greșite");
+        //     } else if (!res.data.enabled) {
+        //         toast.info("Vă rugăm să verificați adresa de email");
+        //         setGresit(false);
+        //     } else {
+        //         localStorage.setItem("user", JSON.stringify(res.data));
+        //         navigate("/");
+        //     }
+        // });
+    };
+
+    const handleForgot = () => {
+        // if (email === "") toast.info("Vă rugăm introduceți adresa de email");
+        // else {
+        //     sendEmailReset(email);
+        //     toast.success("Mail-ul a fost trimis la adresa dumneavoastră");
+        // }
+    };
+
+    return (
+        <div className="login">
+            <div className="center">
+                <h1>Autentificare</h1>
+                <form method="post">
+                    <div className="txt_field">
+                        <input
+                            className="fill"
+                            type="text"
+                            name="Email"
+                            value={email}
+                            errorr={gresit.toString()}
+                            required
+                            onChange={(e) => setEmail(e.target.value)}
+                        />
+                        <label>Email</label>
+                    </div>
+
+                    <div className="txt_field">
+                        <input
+                            className="fill"
+                            type="password"
+                            name="password"
+                            value={parola}
+                            errorr={gresit.toString()}
+                            required
+                            onChange={(e) => setParola(e.target.value)}
+                        />
+                        <label>Parolă</label>
+                    </div>
+                    <div className="pass" onClick={handleForgot}>
+                        Ai uitat parola?
+                    </div>
+                    <input type="button" value="Autentificare" onClick={handleSubmit}></input>
+                    <div className="signup_link">
+                        Nu ai cont?
+                        <a href="/register">Creează cont</a>
+                    </div>
+                </form>
+            </div>
+            <ToastContainer
+                position="top-center"
+                autoClose={5000}
+                hideProgressBar={false}
+                newestOnTop={false}
+                closeOnClick
+                rtl={false}
+                pauseOnFocusLoss
+                draggable
+                pauseOnHover
+                theme="light"
+            />
+        </div>
+    );
+}
+
+export default Login;
