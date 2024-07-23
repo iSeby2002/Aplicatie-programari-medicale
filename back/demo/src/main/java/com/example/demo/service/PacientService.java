af2bf05e835ccdbccc521092095d69dd03bae7db
deliacostea <126463814+deliacostea@users.noreply.github.com>
2024-07-23 14:02:30 +0300

    Delete back directory

--- back/demo/src/main/java/com/example/demo/service/PacientService.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.service;
-
-import lombok.Setter;
-import org.springframework.stereotype.Component;
-import org.springframework.stereotype.Service;
-
-@Component
-public interface PacientService {
-}
